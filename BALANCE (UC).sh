dcacd588f022b855d7fdbfbd601efaefc1904ad5
Rve27 <radikagoo123@gmail.com>
2024-04-16 06:46:20 +0700

    Atomic (Balance UC): rename CPU Input Boost PATH

--- BALANCE (UC).sh
+++ BALANCE (UC).sh
@@ -16,7 +16,7 @@ RVE_CPU0_FREQ=/sys/devices/system/cpu/cpu0/cpufreq
 RVE_CPU0_RvKernel=/sys/devices/system/cpu/cpu0/cpufreq/rvkernel
 RVE_CPU0_CORE=/sys/devices/system/cpu/cpu0/core_ctl
 RVE_CPU_BOOST=/sys/module/cpu_boost/parameters
-RVE_CPUib=/sys/module/cpu_input_boost/parameters
+RVE_CPU_INPUT_BOOST=/sys/module/cpu_input_boost/parameters
 
 RVE_CPU4_FREQ=/sys/devices/system/cpu/cpu4/cpufreq
 RVE_CPU4_RvKernel=/sys/devices/system/cpu/cpu4/cpufreq/rvkernel
@@ -88,13 +88,13 @@ chmod 0644 "$RVE_CPU4_CORE/min_cpus"
 chmod 0644 "$RVE_CPU_BOOST/input_boost_freq"
 chmod 0644 "$RVE_CPU_BOOST/input_boost_ms"
 chmod 0644 "$RVE_CPU_BOOST/sched_boost_on_input"
-chmod 0644 "$RVE_CPUib/dynamic_stune_boost"
-chmod 0644 "$RVE_CPUib/dynamic_stune_boost_duration"
-chmod 0644 "$RVE_CPUib/input_boost_duration"
-chmod 0644 "$RVE_CPUib/remove_input_boost_freq_lp"
-chmod 0644 "$RVE_CPUib/input_boost_freq_lp"
-chmod 0644 "$RVE_CPUib/remove_input_boost_freq_perf"
-chmod 0644 "$RVE_CPUib/input_boost_freq_hp"
+chmod 0644 "$RVE_CPU_INPUT_BOOST/dynamic_stune_boost"
+chmod 0644 "$RVE_CPU_INPUT_BOOST/dynamic_stune_boost_duration"
+chmod 0644 "$RVE_CPU_INPUT_BOOST/input_boost_duration"
+chmod 0644 "$RVE_CPU_INPUT_BOOST/remove_input_boost_freq_lp"
+chmod 0644 "$RVE_CPU_INPUT_BOOST/input_boost_freq_lp"
+chmod 0644 "$RVE_CPU_INPUT_BOOST/remove_input_boost_freq_perf"
+chmod 0644 "$RVE_CPU_INPUT_BOOST/input_boost_freq_hp"
 
 #GPU Permission
 chmod 0644 "$RVE_GPU/min_clock_mhz"
@@ -195,13 +195,13 @@ echo 64 > "$RVE_CPU_BOOST/input_boost_ms"
 echo 1 > "$RVE_CPU_BOOST/sched_boost_on_input"
 
 #CPU Input Boost
-echo 1 > "$RVE_CPUib/dynamic_stune_boost"
-echo 64 > "$RVE_CPUib/dynamic_stune_boost_duration"
-echo 64 > "$RVE_CPUib/input_boost_duration"
-echo 300000 > "$RVE_CPUib/remove_input_boost_freq_lp"
-echo 748800 > "$RVE_CPUib/input_boost_freq_lp"
-echo 825600 > "$RVE_CPUib/remove_input_boost_freq_perf"
-echo 1056000 > "$RVE_CPUib/input_boost_freq_hp"
+echo 1 > "$RVE_CPU_INPUT_BOOST/dynamic_stune_boost"
+echo 64 > "$RVE_CPU_INPUT_BOOST/dynamic_stune_boost_duration"
+echo 64 > "$RVE_CPU_INPUT_BOOST/input_boost_duration"
+echo 300000 > "$RVE_CPU_INPUT_BOOST/remove_input_boost_freq_lp"
+echo 748800 > "$RVE_CPU_INPUT_BOOST/input_boost_freq_lp"
+echo 825600 > "$RVE_CPU_INPUT_BOOST/remove_input_boost_freq_perf"
+echo 1056000 > "$RVE_CPU_INPUT_BOOST/input_boost_freq_hp"
 
 #GPU
 echo 160 > "$RVE_GPU/min_clock_mhz"
