a1aeb5b5b9ea232f5903af1b7c3ac6cb479c45c2
Rve27 <radikagoo123@gmail.com>
2024-04-16 23:18:37 +0700

    Atomic (RvK-G840): change GPU max freq & min freq

--- RvK-G840.sh
+++ RvK-G840.sh
@@ -200,17 +200,17 @@ echo 1536000 > "$RVE_CPU_INPUT_BOOST/remove_input_boost_freq_perf"
 echo 1536000 > "$RVE_CPU_INPUT_BOOST/input_boost_freq_hp"
 
 # GPU
-echo 160 > "$RVE_GPU/min_clock_mhz"
-echo 710 > "$RVE_GPU/max_clock_mhz"
-echo 710000000 > "$RVE_GPU/max_gpuclk"
+echo 414 > "$RVE_GPU/min_clock_mhz"
+echo 840 > "$RVE_GPU/max_clock_mhz"
+echo 840000000 > "$RVE_GPU/max_gpuclk"
 echo 1 > "$RVE_GPU/bus_split"
 echo 0 > "$RVE_GPU/force_bus_on"
 echo 0 > "$RVE_GPU/force_rail_on"
 echo 0 > "$RVE_GPU/force_clk_on"
 echo 0 > "$RVE_GPU/throttling"
 
-echo 160000000 > "$RVE_GPU_FREQ/min_freq"
-echo 710000000 > "$RVE_GPU_FREQ/max_freq"
+echo 414000000 > "$RVE_GPU_FREQ/min_freq"
+echo 840000000 > "$RVE_GPU_FREQ/max_freq"
 echo 1 > "$RVE_GPU_FREQ/adrenoboost"
 
 # I/O Scheduler
